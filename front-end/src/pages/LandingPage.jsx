80c98c58f4b47a6d433cbf367818fffd51e1de98
Akash <akashhedaoo26782@gmail.com>
2025-07-19 15:47:32 +0530

    navbar routing and landing page building

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import "../styles/LandingPage.css";
+import landingpageimg from "../assets/images/landingpagephoto.jpg";
 
 const LandingPage = () => {
   return (
-    <div style={{ minHeight: '80vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ fontSize: '2.5rem', color: '#a500a0ff', marginBottom: '1rem' }}>Welcome to SaloonGo</h1>
-      <p style={{ fontSize: '1.2rem', color: '#333', maxWidth: 600, textAlign: 'center' }}>
-        Discover and book the best salons near you. Enjoy easy appointment booking, real-time crowd updates, and more!
-      </p>
+    <div className="landing-page-container">
+      <div className="image-container">
+        <img src={landingpageimg} alt="Landing" />
+      </div>
+      <div className="imgtext">
+        <h1>Book your next saloon appointment with ease</h1>
+        <h4>SaloonGo connects you with nearby salons, offering real-time queue updates and seamless booking.</h4>
+      </div>
     </div>
   );
 };
