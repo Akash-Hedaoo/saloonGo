868a270843a1826486c94716c69dea01099b3b21
Akash <akashhedaoo26782@gmail.com>
2025-07-19 16:50:10 +0530

    dark mode enabled for the navbar and home page minor changes

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -10,8 +10,26 @@ const LandingPage = () => {
       </div>
       <div className="imgtext">
         <h1>Book your next saloon appointment with ease</h1>
-        <h4>SaloonGo connects you with nearby salons, offering real-time queue updates and seamless booking.</h4>
+        <h4>
+          SaloonGo connects you with nearby salons, offering real-time queue
+          updates and seamless booking.
+        </h4>
+        <button>Book Slot</button>
       </div>
+      <div className="midpart">
+        <h1>Key Features</h1>
+        <h5>Experience the convenience of SaloonGo with these essential features</h5>
+        <div className="featurecontainer">
+            <div className="feature"></div>
+            <div className="feature"></div>
+            <div className="feature"></div>
+        </div>
+      </div>
+      <div className="landing-page-footer">
+        <h1>Ready to transform your saloon experience?</h1>
+        
+      </div>
+      <div className="footer">&copy; : 2025 All rights reserved</div>
     </div>
   );
 };
