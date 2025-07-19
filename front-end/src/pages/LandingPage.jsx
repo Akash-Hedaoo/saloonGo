0369c9482acb8b9a69da57af030d00710f58ce59
Akash <akashhedaoo26782@gmail.com>
2025-07-19 17:38:10 +0530

    minor changes in the landing page

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -17,12 +17,27 @@ const LandingPage = () => {
         <button>Book Slot</button>
       </div>
       <div className="midpart">
-        <h1>Key Features</h1>
+        <div className="sidetext">
+             <h1>Key Features</h1>
         <h5>Experience the convenience of SaloonGo with these essential features</h5>
+        </div>
+       
         <div className="featurecontainer">
-            <div className="feature"></div>
-            <div className="feature"></div>
-            <div className="feature"></div>
+            <div className="feature">
+                <img src="" alt="" />
+                <h1>Easy Booking</h1>
+                <p>Schedule appointments effortlessly with just a few taps.</p>
+            </div>
+            <div className="feature">
+                 <img src="" alt="" />
+                <h1>Real-Time Queue Updates</h1>
+                <p>Stay informed with live updates on your appointment status.</p>
+            </div>
+            <div className="feature">
+                 <img src="" alt="" />
+                <h1>Find Nearby Salons</h1>
+                <p>Discover top-rated salons in your area with our intuitive map.</p>
+            </div>
         </div>
       </div>
       <div className="landing-page-footer">
